c1eff4fe4f1a221ef16f8250b841dc09fb96bfd5
d-pettersson <david.pettersson@code.berlin>
2021-10-25 14:35:48 +0200

    Add common library

--- src/common.py
+++ src/common.py
@@ -3,18 +3,24 @@ if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
-# global variables for base_layer_0
-table_pars = op('table_pars')
-table_pars_mapped = op('table_pars_mapped')
-
 # common functions
+def mapTableRange(channel, val, table, mapped_table):
+	for index in range(1, table.numRows):
+		if channel.name == table[index, 'par_name']:
+			mapped_table[index - 1, 0] = table[index, 'par_name']
+			if isInteger(table[index, 'min']):
+				mapped_table[index - 1, 1] = int(
+					round(mapRange(val, 0, 1, table[index, 'min'], table[index, 'max'])))
+			else:
+				mapped_table[index - 1, 1] = mapRange(val, 0, 1, table[index, 'min'], table[index, 'max'])
+
 def mapRange(value, inMin, inMax, outMin, outMax):
-    return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
+	return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
 
 def isInteger(n):
-    try:
-        float(n)
-    except ValueError:
-        return False
-    else:
-        return float(n).isInteger()
+	try:
+		float(n)
+	except ValueError:
+		return False
+	else:
+		return float(n).is_integer()
