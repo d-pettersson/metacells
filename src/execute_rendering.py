0c41a3b328be0aa736370d9eb141d3efddb90f63
d-pettersson <david.pettersson@code.berlin>
2021-10-26 14:02:12 +0200

    Refactor rendering - clean up folder paths - add ui toggle for rendering - add realtime toggle

--- src/execute_rendering.py
+++ src/execute_rendering.py
@@ -13,11 +13,19 @@ if False:
 import os
 import subprocess
 
-console = op('text_console')
+from datetime import datetime
+
+# settings
+ffmpegPath = os.path.join(os.curdir, 'ffmpeg', 'bin', 'ffmpeg.exe')
+
+# TD ops
+isRecording = op('container_ui/buttonToggle_record').par.Value0
+content = op('container_ui/text_content')
 masterSeed = op('constant_master_seed')
 exportNull = op('null_export')
-name = 'serialCore'
 
+# variables
+name = 'serialCore'
 counter = 0
 seed = 0
 
@@ -33,27 +41,34 @@ def onExit():
 def onFrameStart(frame):
 	global counter
 	global seed
+
 	seed = int(op('constant_master_seed')['seed'])
 
-	rootFolder = os.path.realpath(str(op('folder1').par.rootfolder))
-	seedFolder = os.path.join(rootFolder, name + '.')
-	seedFolder += str(seed)
+	renderFolder = os.path.join(os.curdir, 'render')
+	imgFolder = os.path.join(renderFolder, 'img-sequence')
+	videoFolder = os.path.join(renderFolder, 'video')
 
-	if counter % 2 != 0:
-		exportNull.save(os.path.join(seedFolder, name) + '.' + str(seed) + '.' + str(int(frame)) + '.tif', createFolders = True)
-		if frame == 1.0:
-			console.par.text += '[✓] saving img sequence: serialCore.{0}\n'.format(seed)
+	seedFolder = os.path.join(imgFolder, name)
+	seedFolder += '.{}'.format(str(seed))
 
-	if frame == me.time.rangeEnd:
+	if isRecording:
+		# save individual images on frameStart on second timeline pass
 		if counter % 2 != 0:
-			console.par.text += '[✓] rendering: serialCore.{0}.mp4'.format(seed)
-			cmd = "./ffmpeg/bin/ffmpeg.exe -y -framerate 60 -i " + seedFolder + "/serialCore." + str(seed) +".%d.tif -c:v libx264 -crf 18 -pix_fmt yuv420p ./render/video/serialCore." + str(seed) + ".mp4"
-			process = subprocess.Popen(cmd, shell=False)
-			seed += 1
-			masterSeed.par.value0 = seed
-			# result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-			# print(result.returncode, result.stdout, result.stderr)'
-		counter += 1
+			exportNull.save(os.path.join(seedFolder, name) + '.{0}.{1}.tif'.format(str(seed), str(int(frame))), createFolders = True)
+			if frame == 1.0:
+				content.text += '[✓] saving img sequence: serialCore.{0}\n'.format(seed)
+
+		# render image sequence to video once timeline is finished on second timeline pass
+		# iterate to next seed
+		if frame == me.time.rangeEnd:
+			if counter % 2 != 0:
+				content.text += '[✓] rendering: serialCore.{0}.mp4\n'.format(seed)
+				cmd = '{0} -y -framerate 60 -i {1}.{2}.%d.tif -c:v libx264 -crf 18 -pix_fmt yuv420p {3}.{2}.mp4'\
+					.format(ffmpegPath, os.path.join(seedFolder, name), str(seed), os.path.join(videoFolder, name))
+				subprocess.Popen(cmd, shell=False)
+				seed += 1
+				masterSeed.par.value0 = seed
+			counter += 1
 
 	return
 
@@ -61,6 +76,14 @@ def onFrameEnd(frame):
 	return
 
 def onPlayStateChange(state):
+	global counter
+	counter = 0
+
+	if isRecording and state:
+		project.realTime = False
+		content.text += 'Render protocol initiated\nTime: {0}\n'.format(datetime.now())
+	else:
+		project.realTime = True
 	return
 
 def onDeviceChange():
